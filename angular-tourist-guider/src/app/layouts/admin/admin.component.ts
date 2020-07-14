5509a6c5dc6ac4e51abe7797639e5d0dc06492df
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-07-14 13:05:26 +0900

    added Angular Material

--- angular-tourist-guider/src/app/layouts/admin/admin.component.ts
+++ angular-tourist-guider/src/app/layouts/admin/admin.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
+  mode = new FormControl('side');
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- angular-tourist-guider/src/app/layouts/admin/admin.module.ts
+++ angular-tourist-guider/src/app/layouts/admin/admin.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatRadioModule } from '@angular/material/radio';
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSidenavModule,
+    MatRadioModule,
+  ],
 })
-export class AdminModule { }
+export class AdminModule {}
