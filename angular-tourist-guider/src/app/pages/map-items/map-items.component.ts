1c9d3a040438832b670e777036cef8ba85b8eaf2
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-07-19 04:47:31 +0900

    added dummy data

--- angular-tourist-guider/src/app/pages/map-items/map-items.component.ts
+++ angular-tourist-guider/src/app/pages/map-items/map-items.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { mapItems } from '../../../assets/dummy-data/dummy-items.js';
 
 @Component({
   selector: 'app-map-items',
   templateUrl: './map-items.component.html',
-  styleUrls: ['./map-items.component.scss']
+  styleUrls: ['./map-items.component.scss'],
 })
 export class MapItemsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
+  public mapItems = mapItems;
   ngOnInit(): void {
+    console.log(this.mapItems);
   }
-
 }
---
+++ angular-tourist-guider/src/assets/dummy-data/dummy-items.js
@@ -0,0 +1,38 @@
+export const mapItems = [
+  {
+    name: "Restaurant El Drago",
+    category: "foods/drinks",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+  },
+  {
+    name: "Sr. Frago Park",
+    category: "parks/sights",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+  },
+  {
+    name: "Hotel Nurkikso",
+    category: "sleep/rest",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+  },
+  {
+    name: "Basket Ball Tournament",
+    category: "sports show",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+  },
+  {
+    name: "Singing Contest",
+    category: "variety show",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+  },
+  {
+    name: "Beauty Contest",
+    category: "variety show",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+  },
+];
