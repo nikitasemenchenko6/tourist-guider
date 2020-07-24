f60f00c226fadb405987c5d1ce5caa8bb1ffe6d4
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-07-24 12:30:30 +0900

    added multiple item

--- angular-tourist-guider/src/assets/dummy-data/dummy-items.js
+++ angular-tourist-guider/src/assets/dummy-data/dummy-items.js
@@ -4,35 +4,55 @@ export const mapItems = [
     category: "foods/drinks",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/08/13",
   },
   {
     name: "Sr. Frago Park",
     category: "parks/sights",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/06/21",
   },
   {
     name: "Hotel Nurkikso",
     category: "sleep/rest",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/07/13",
   },
   {
     name: "Basket Ball Tournament",
     category: "sports show",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/09/13",
   },
   {
     name: "Singing Contest",
     category: "variety show",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/07/11",
   },
   {
     name: "Beauty Contest",
     category: "variety show",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/07/24",
+  },
+  {
+    name: "Dancing Contest",
+    category: "variety show",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/07/11",
+  },
+  {
+    name: "Eating Contest",
+    category: "variety show",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nlputate. Lorem ipsum dolor...",
+    created: "2020/07/24",
   },
 ];
