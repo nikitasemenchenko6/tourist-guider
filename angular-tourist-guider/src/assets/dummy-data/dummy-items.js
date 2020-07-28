9ec5cbbad1c4256d5b95274d0195663c12547cdb
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-07-28 22:16:44 +0900

    added id to demo items

--- angular-tourist-guider/src/assets/dummy-data/dummy-items.js
+++ angular-tourist-guider/src/assets/dummy-data/dummy-items.js
@@ -1,5 +1,6 @@
 export const mapItems = [
   {
+    id: 1,
     name: "Restaurant El Drago",
     category: "foods/drinks",
     description:
@@ -7,6 +8,7 @@ export const mapItems = [
     created: "2020/08/13",
   },
   {
+    id: 2,
     name: "Sr. Frago Park",
     category: "parks/sights",
     description:
@@ -14,6 +16,7 @@ export const mapItems = [
     created: "2020/06/21",
   },
   {
+    id: 3,
     name: "Hotel Nurkikso",
     category: "sleep/rest",
     description:
@@ -21,6 +24,7 @@ export const mapItems = [
     created: "2020/07/13",
   },
   {
+    id: 4,
     name: "Basket Ball Tournament",
     category: "sports show",
     description:
@@ -28,6 +32,7 @@ export const mapItems = [
     created: "2020/09/13",
   },
   {
+    id: 5,
     name: "Singing Contest",
     category: "variety show",
     description:
@@ -35,6 +40,7 @@ export const mapItems = [
     created: "2020/07/11",
   },
   {
+    id: 6,
     name: "Beauty Contest",
     category: "variety show",
     description:
@@ -42,6 +48,7 @@ export const mapItems = [
     created: "2020/07/24",
   },
   {
+    id: 7,
     name: "Dancing Contest",
     category: "variety show",
     description:
@@ -49,6 +56,7 @@ export const mapItems = [
     created: "2020/07/11",
   },
   {
+    id: 8,
     name: "Eating Contest",
     category: "variety show",
     description:
@@ -56,6 +64,7 @@ export const mapItems = [
     created: "2020/07/24",
   },
   {
+    id: 9,
     name: "Flying Contest",
     category: "variety show",
     description:
@@ -63,6 +72,7 @@ export const mapItems = [
     created: "2020/07/24",
   },
   {
+    id: 10,
     name: "Jumping Contest",
     category: "variety show",
     description:
@@ -70,6 +80,7 @@ export const mapItems = [
     created: "2020/07/11",
   },
   {
+    id: 11,
     name: "Swiming Contest",
     category: "variety show",
     description:
