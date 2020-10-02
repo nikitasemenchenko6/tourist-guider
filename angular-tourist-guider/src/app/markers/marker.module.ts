3eb32bef9a3b0e0f405c51530feb2893b659d04e
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-10-03 01:32:33 +0900

    change to createReducer

--- angular-tourist-guider/src/app/markers/marker.module.ts
+++ angular-tourist-guider/src/app/markers/marker.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StoreModule } from '@ngrx/store';
-import { MapMarkerReducer } from 'src/app/markers/state/map-marker.reducer';
+import { mapMarkerReducer } from 'src/app/markers/state/map-marker.reducer';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forFeature('mapMarkers', MapMarkerReducer),
+    StoreModule.forFeature('mapMarkers', mapMarkerReducer),
   ],
   exports: [],
 })
--- angular-tourist-guider/src/app/markers/state/map-marker.reducer.ts
+++ angular-tourist-guider/src/app/markers/state/map-marker.reducer.ts
@@ -1,13 +1,8 @@
 // import { ItemActionTypes } from 'src/app/shared/enum/item-action-types';
-import { ActionParent } from 'src/app/actions/map-item.action';
+import { createReducer } from '@ngrx/store';
 import { MapMarker } from 'src/app/models/MapMarker';
 import { mapItems } from 'src/assets/dummy-data/dummy-items.js';
 
 export const initState: MapMarker[] = mapItems;
 
-export function MapMarkerReducer(state = initState, action: ActionParent) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
+export const mapMarkerReducer = createReducer<MapMarker[]>(initState);
