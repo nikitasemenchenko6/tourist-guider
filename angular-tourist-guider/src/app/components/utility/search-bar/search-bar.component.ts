797d7c6b864ac87a6128957c77df1414293fc4cd
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-10-18 14:59:12 +0900

    added search filter dispatch action

--- angular-tourist-guider/src/app/components/utility/search-bar/search-bar.component.ts
+++ angular-tourist-guider/src/app/components/utility/search-bar/search-bar.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { searchFilter } from 'src/app/markers/state/map-marker.action';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void { }
 
   searching(event: Event) {
     const output = event.target as HTMLInputElement
-    console.log(output.value)
+    // console.log(output.value)
+    this.store.dispatch(searchFilter({ text: output.value }))
   }
 }
