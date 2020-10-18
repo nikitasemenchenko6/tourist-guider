09ab2e5d06a968fc3f8d3c56757c03d02918c311
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-10-18 14:58:11 +0900

    created action searchFilter

--- angular-tourist-guider/src/app/markers/state/map-marker.action.ts
+++ angular-tourist-guider/src/app/markers/state/map-marker.action.ts
@@ -4,3 +4,8 @@ export const deleteMarker = createAction(
   '[MAP-MARKER DELETED] Toggle Trash Icon',
   props<{ id: Number }>()
 );
+
+export const searchFilter = createAction(
+  '[MAP-MARKER SEARCH FILTER] input in searchbar',
+  props<{ text: string }>()
+)
