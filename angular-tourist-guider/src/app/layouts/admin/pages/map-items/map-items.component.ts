e368ee1a703a141f1fb5a896375abe34420aa977
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-10-18 16:20:34 +0900

    Apply searchFilter && searchFilterPipe

--- angular-tourist-guider/src/app/layouts/admin/pages/map-items/map-items.component.ts
+++ angular-tourist-guider/src/app/layouts/admin/pages/map-items/map-items.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { MapMarker } from 'src/app/models/MapMarker';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { getMarkers } from 'src/app/markers/state/map-marker.reducer';
+import { getMarkers, getSearchFilter } from 'src/app/markers/state/map-marker.reducer';
 import { deleteMarker } from 'src/app/markers/state/map-marker.action';
 
 @Component({
@@ -14,12 +14,18 @@ import { deleteMarker } from 'src/app/markers/state/map-marker.action';
 export class MapItemsComponent implements OnInit {
   p: number = 1;
   mapItems: Observable<MapMarker[]>;
+  searchFilter: string;
 
   constructor(private store: Store<{ mapMarkers: MapMarker[] }>) { }
 
   ngOnInit(): void {
+    /**  we get markers for mapItems */
     this.mapItems = this.store.select(getMarkers);
     // this.mapItems.subscribe((e) => console.log(e));
+    /** we get the texts for search filtered */
+    this.store.select(getSearchFilter).subscribe(item => {
+      this.searchFilter = item;
+    });
   }
 
   deleteItem(id: number) {
