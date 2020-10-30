1ed4bf05df8be3989338d35d1eccb434e0e759dc
nikitasemenchenko6 <nikitasemenchenko6@gmail.com>
2020-10-30 23:56:16 +0900

    added matinput module

--- angular-tourist-guider/src/app/layouts/admin/admin.module.ts
+++ angular-tourist-guider/src/app/layouts/admin/admin.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
 // import { BrowserModule } from '@angular/platform-browser';
 import { NgxPaginationModule } from 'ngx-pagination';
 
@@ -39,6 +40,7 @@ import { MapMarkerModule } from 'src/app/markers/map-marker.module';
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
+    MatInputModule,
     // BrowserModule,
     NgxPaginationModule,
     MapMarkerModule,
